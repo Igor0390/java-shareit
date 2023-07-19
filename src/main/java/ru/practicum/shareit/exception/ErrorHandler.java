27c535cfec857e99d6d72e74b3b48d0a6186d8ca
Igor0390 <118315204+Igor0390@users.noreply.github.com>
2023-07-19 23:06:17 +0200

    изменения по стилю

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -26,6 +26,7 @@ public class ErrorHandler {
         log.error("Validation Exception");
         return new ErrorResponse(e.getMessage());
     }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleValidate(final ErrorValidation e) {

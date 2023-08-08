41a8bca6bdfd2ab759d468c2654e50fe0bbb42ac
Igor0390 <118315204+Igor0390@users.noreply.github.com>
2023-08-08 22:24:00 +0200

    Первый комит в add-bookings

---
+++ src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -0,0 +1,52 @@
+package ru.practicum.shareit.booking;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingRequestDto;
+import ru.practicum.shareit.booking.dto.BookingShortDto;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class BookingMapper {
+    public static BookingDto toBookingDto(Booking booking) {
+        return BookingDto.builder()
+                .id(booking.getId())
+                .start(booking.getStart())
+                .end(booking.getEnd())
+                .booker(booking.getBooker())
+                .status(booking.getStatus())
+                .item(booking.getItem())
+                .build();
+    }
+
+    public static Booking toBooking(BookingRequestDto bookingDto, User user, Item item) {
+        return Booking.builder()
+                .start(bookingDto.getStart())
+                .end(bookingDto.getEnd())
+                .booker(user)
+                .item(item)
+                .build();
+    }
+
+
+    public static BookingShortDto toBookingShortDto(Booking booking) {
+        return BookingShortDto.builder()
+                .id(booking.getId())
+                .start(booking.getStart())
+                .end(booking.getEnd())
+                .bookerId(booking.getBooker().getId())
+                .status(booking.getStatus())
+                .build();
+    }
+
+    public static List<BookingDto> toBookingsDto(List<Booking> bookings) {
+        return bookings.stream()
+                .map(BookingMapper::toBookingDto)
+                .collect(Collectors.toList());
+    }
+}
--- src/main/java/ru/practicum/shareit/booking/dto/BookingShortDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingShortDto.java
@@ -2,29 +2,24 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.*;
 import ru.practicum.shareit.booking.model.Status;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.validation.constraints.Future;
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Builder
-public class BookingDto {
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingShortDto {
     private Long id;
     @NotNull
-    @FutureOrPresent
     private LocalDateTime start;
     @NotNull
-    @Future
     private LocalDateTime end;
-    @NotNull
-    private Item item;
-    private User booker;
+    private Long bookerId;
     @Enumerated(value = EnumType.STRING)
     private Status status;
 }

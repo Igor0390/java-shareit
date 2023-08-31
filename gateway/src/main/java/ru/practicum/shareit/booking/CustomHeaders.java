11fb6767e0c205eb42e4d66308cb54fba8c95a37
Igor0390 <118315204+Igor0390@users.noreply.github.com>
2023-08-31 22:54:39 +0200

    изменения стиля

--- gateway/src/main/java/ru/practicum/shareit/booking/CustomHeaders.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/CustomHeaders.java
@@ -1,5 +1,5 @@
 package ru.practicum.shareit.booking;
 
 public class CustomHeaders {
-    public static final String X_SHARER_USER_ID = "X-Sharer-User-Id";
+    public static final String X_SHARER_USER_ID = "X-SHARER-USER-ID";
 }
